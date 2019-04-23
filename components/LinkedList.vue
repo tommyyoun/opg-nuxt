977a90e59884bae7a6d2a16f255729a825225859
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2019-04-23 16:09:26 -0700

    Adds LinkedList component with styling for list

---
+++ components/LinkedList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="list-wrapper">
+           <h2 class="title">Link List Component</h2>
+           <!-- Swap 'regular' class with 'numbered' to switch list-style-type -->
+              <ol class="links-indicator regular">
+                <li><a href="">Another Road to Safety (ARS)</a></li>
+                <li><a href="">Assignment of referrals</a></li>
+                <li><a href="">Authorities and references for the ER program</a></li>
+                <li><a href="">Child Abuse and Neglect Reporting Act </a></li>
+                <li><a href="">Confidentiality and ER</a></li>
+                <li><a href="">Definitions of abuse and neglect</a></li>
+                <li><a href="">ER worker rules &amp; equipment</a></li>
+                <li><a href="">Medical neglect</a></li>
+                <li><a href="">Purpose &amp; function of the ER program</a></li>
+                <li><a href="">Safely Surrendered Baby Protocol</a></li>
+                <li><a href="">Sensitive referrals and cases </a></li>
+                <li><a href="">Standby / Call Back (ER After Hours) procedure</a></li>
+                <li><a href="">Transporting children</a></li>
+              </ol>
+    </div>         
+</template>
+
+<script>
+    
+</script>
+
+<style>
+  .links-indicator {
+    border-left: 3px solid #3C78B4;
+  }
+
+  .list-wrapper {
+    position: relative;
+  }
+  .regular {
+    list-style-type: initial;
+  }
+  .numbered {
+    list-style-type: decimal;
+  }
+
+</style>
