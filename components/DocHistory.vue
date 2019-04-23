ea499df76fc9a7dd3c404b6a01c7b6d94d944efc
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2019-04-22 17:13:52 -0700

    Adds some responsive classes fixes update-note

--- components/DocHistory.vue
+++ components/DocHistory.vue
@@ -6,34 +6,10 @@
 
           <li>
             <div class="doc-row-wrapper">
-                <div class="doc-name"><span>Document Name</span></div>
-                <span>changed on:</span>
-                <div class="update-time"><strong><time>04/12/2019</time></strong></div>
-                <div class="update-note"><strong><span>Some things were changed on this doc.</span></strong></div>
-            </div>
-          </li>
-          <li>
-            <div class="doc-row-wrapper">
-                <div class="doc-name"><span>Document Name</span></div>
-                <span>changed on:</span>
-                <div class="update-time"><strong><time>04/12/2019</time></strong></div>
-                <div class="update-note"><strong><span>Some things were changed on this doc.</span></strong></div>
-            </div>
-          </li>
-          <li>
-            <div class="doc-row-wrapper">
-                <div class="doc-name"><span>Document Name</span></div>
-                <span>changed on:</span>
-                <div class="update-time"><strong><time>04/12/2019</time></strong></div>
-                <div class="update-note"><strong><span>Some things were changed on this doc.</span></strong></div>
-            </div>
-          </li>
-          <li>
-            <div class="doc-row-wrapper">
-                <div class="doc-name"><span>Document Name</span></div>
-                <span>changed on:</span>
-                <div class="update-time"><strong><time>04/12/2019</time></strong></div>
-                <div class="update-note"><strong><span>Some things were changed on this doc.</span></strong></div>
+                <div class="doc-name"><strong>Document Name</strong></div>
+                <div><span>changed on:</span></div>
+                <div class="update-time"><strong><time>04-12-2019</time></strong></div>
+                <div class="update-note"><strong><span>Some things were changed on this doc.Some things were changed on this doc.Some things were changed on this doc.Some things were changed on this doc.Some things were changed on this doc.Some things were changed on this doc.Some things were changed on this doc.</span></strong></div>
             </div>
           </li>
 
@@ -48,23 +24,53 @@
 <style>
 .doc-row-wrapper {
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     font-style: italic;
+    flex-wrap: wrap;
 }
 .doc-list {
     list-style-type: none;
 }
+.doc-name {
+    margin-right: 1.5rem;
+}
+.doc-name + div {
+    margin-right: 1.5rem;
+}
 .update-time {
     color: orangered;
+    margin-right: 1.5rem;
+    
 }
 .update-note {
-    color: limegreen;
+    color: forestgreen;
+    max-width: 50%;
 }
 .doc-list li {
     margin: 1em 0em;
 }
 .doc-list li:nth-child(even){
     background-color:aqua;
+}
+
+@media only screen and (max-width: 576px) {
+    .doc-row-wrapper {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        font-style: italic;
+        margin-right: 2rem;
+    }
+    .doc-name {
+        border-top: 1px dashed black;
+        padding-top: .5rem;
+    }
+    .update-note {
+        max-width: 100%;
+        padding-bottom: .5rem;
+        border-bottom: 1px dashed black;
+    }
 }
 
 </style>
