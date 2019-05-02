8c4f8dcc1b4838a132cd151a05157865bdfec6d9
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2019-05-02 13:44:55 -0700

    Adds #backtotop CSS

--- components/MainContainer.vue
+++ components/MainContainer.vue
@@ -50,5 +50,38 @@ export default {
 </script>
 
 <style>
+ #backtop {
+    position: fixed;
+    left: auto;
+    right: 20px;
+    top: auto;
+    bottom: 20px;
+    outline: none;
+    overflow: hidden;
+    color: #fff;
+    text-align: center;
+    background-color: rgba(154, 205, 50, 0.84);
+    height: 40px;
+    width: 40px;
+    line-height: 40px;
+    font-size: 14px;
+    border-radius: 2px;
+    cursor: pointer;
+    transition: all 0.3s linear;
+    z-index: 999999;
+
+    opacity: 1;
+    /* display: none; */
+}
+
+#backtop:hover {
+    background-color: #9ACD32;
+}
+
+#backtop.mcOut {
+    opacity: 0;
+}
+
+
 
 </style>
